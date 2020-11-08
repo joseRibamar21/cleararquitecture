da540449d58a14158c524cb7802c32d4020daa05
joseRibamar21 <45271784+joseRibamar21@users.noreply.github.com>
2020-11-08 18:35:57 -0300

    aula 21

---
+++ test/presentation/presenters/stream_login_presentere_test.dart
@@ -0,0 +1,42 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:cleanarquiteture2/presenter/presenters/presenters.dart';
+import 'package:cleanarquiteture2/presenter/protocols/protocols.dart';
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  StreamLoginPresenter sut;
+  ValidationSpy validation;
+  String email;
+
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+    mockValidation();
+  });
+
+  test('Should call Validation with correct email', () {
+    sut.validadeEmail(email);
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    mockValidation(value:'error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validadeEmail(email);
+  });
+}
