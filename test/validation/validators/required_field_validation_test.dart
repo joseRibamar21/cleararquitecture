787594daf2b5d583af7cc3792838795511263d3d
joseRibamar21 <45271784+joseRibamar21@users.noreply.github.com>
2020-11-18 11:38:56 -0300

    aula 24

---
+++ test/validation/validators/required_field_validation_test.dart
@@ -0,0 +1,21 @@
+import 'package:test/test.dart';
+
+import 'package:cleanarquiteture2/validation/validators/validators.dart';
+
+void main() {
+  RequiredFieldValidation sut;
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
+  test('Should return error if value empty', () {
+    expect(sut.validate('any_value'), null);
+  });
+
+  test('Should return null if value is not empty', () {
+    expect(sut.validate(''), 'Campo obrigatório');
+  });
+
+  test('Should return null if value is null', () {
+    expect(sut.validate(null), 'Campo obrigatório');
+  });
+}
